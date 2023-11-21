e143aef8a46712a01716afe919a87ad5a254cd2a
Jon Hogendorp <jonhoog@live.nl>
2023-11-21 13:09:11 +0100

    first commit diffrent comps set-up

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import Button from "./Button";
+
+function App() {
+    return (
+    <div>
+        <div>
+            <Button primary success>Click me!</Button>
+        </div>
+        <div>
+            <Button>Hello World</Button>
+        </div>
+        <div>
+            <Button>Free beer here</Button>
+        </div>
+        <div>
+            <Button>Buy now</Button>
+        </div>
+        <div>
+            <Button>Hide ads</Button>
+        </div>
+        <div>
+            <Button>pes kati</Button>
+        </div>
+    </div>
+)};
+
+export default App;
